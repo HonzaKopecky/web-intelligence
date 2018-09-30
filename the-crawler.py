a041cec7bd7f91499bdf6ec1ca7971acb55d6f04
Honza Kopecký <honza.kopecky95@gmail.com>
2018-09-30 12:58:12 +0200

    Include Indexer into the tests

--- the-crawler.py
+++ the-crawler.py
@@ -1,10 +1,16 @@
-import lib.crawler
+from lib.crawler import Crawler
+from lib.indexer import Indexer
 
-crawler = lib.crawler.Crawler(10)
+crawler = Crawler(3)
 
-crawler.addSeed("https://hubpraha.cz")
+crawler.addSeed("https://hubpraha.cz/en")
 
 crawler.crawl()
 
 crawler.saveDocuments("out/documents")
-# print(crawler.urlQueue.qsize())
+
+indexer = Indexer()
+
+indexer.indexDocs(crawler.documents)
+
+indexer.printIndex()
